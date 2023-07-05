89778fe1267b6e20e3d76e642f26216e669ba6aa
Patrice M <patriceamardo@gmail.com>
2023-07-05 13:30:00 -0700

    Remove count/state example

--- src/App.js
+++ src/App.js
@@ -1,50 +1,46 @@
 import { useState } from 'react';
 import './App.css';
 
-function MyButton({fun, count}) {
-  return (
-    <button onClick={fun}>Clicked {count} times</button>
-  )
-}
 
 const welcome = {
   name: 'World'
 }
 
-const myList = [{
-  title: 'G',
-  url: 'google.com',
-  objectID: 0}, 
-  {
-    title: 'FB',
-    url: 'facebook.com',
-    objectID: 1
-  }];
-
-  const myListItems = myList.map((item) => {
+const List = ({ items }) => {
+  return items.map((item) => {
     return (
       <div key={item.objectID}>
         <span><a href={item.url}><h3>{item.title}</h3></a></span>
       </div>
     );
   })
-function App() {
+} 
 
-  let [count, setCount] = useState(0);
+const App = () =>  {
+  const stories =  [{
+    title: 'G',
+    url: 'google.com',
+    objectID: 0}, 
+    {
+      title: 'FB',
+      url: 'facebook.com',
+      objectID: 1
+    }];
 
-  function handleClick(){
-    setCount(count+1);
-  }
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value)
+  };
 
   return (
     <>
       <h1>Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange}/>
+      <p>Searching for <strong>{searchTerm}</strong></p>
       <hr />
-      <MyButton count={count} fun={handleClick} />
-      <MyButton count={count} fun={handleClick} />
-    <ul>{myListItems}</ul>
+    <ul><List items={stories}/></ul>
     </>
 );
 }
